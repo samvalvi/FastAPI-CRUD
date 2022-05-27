792dc9110ab92b594238cf55227d272e01e8c41f
samvalvi <57542220+samvalvi@users.noreply.github.com>
2022-05-27 12:27:49 -0600

    env added

--- config/db.py
+++ config/db.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine, MetaData
+from dotenv import load_dotenv
+import os
 
-engine = create_engine('mysql+pymysql://root:Meli8462@localhost:3306/crud_api')
+load_dotenv()
+
+password = os.getenv('MYSQL_ROOT_PASSWORD')
+mysql_db = os.getenv('MYSQL_DATABASE')
+user = os.getenv('MYSQL_ROOT_USER')
+
+engine = create_engine(f'mysql+pymysql://{user}:{password}@localhost:3306/{mysql_db}')
 
 meta = MetaData()
 
